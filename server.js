785afa4bab79fa19eab74d814e0c5a6babbe3734
jcg0 <jg.awsprod@gmail.com>
2023-01-09 14:24:57 -0500

    Ive implimented the POST and GET routes for the /api/notes endpoints, I have been successful in sending the post request and retrieving the get request, last step is to get the new note to render immediately when the post request is sent. right now it only appears on page refresh or if you send a note and then send another. it will show the first note.

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json");
+const uuid = require("./helpers/uuid");
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/notes", (req, res) => {
+  // res.send("<h1>hello world</h1>");
+
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(database);
+  // console.info(database);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text, id } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    let newArr = [];
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      newArr = JSON.parse(data);
+      newArr.push(newNote);
+      console.log(newArr);
+      fs.writeFile("./db/db.json", JSON.stringify(newArr), (err) =>
+        err
+          ? console.error(err)
+          : console.log(`New note added: ${newNote.title}`)
+      );
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("error posting the note");
+  }
+});
+
+app.get("/*", (req, res) => {
+  // res.send("<h1>hello world</h1>");
+  res.sendFile(path.join(__dirname, "/public/index.html"), (err) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", "index.html");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening at https://localhost:${PORT}`);
+});
